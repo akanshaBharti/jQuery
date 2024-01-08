f3f8553f29398f8041cfb6608f645fccfb24bc3e
Akansha <akanshabharti1213@gmail.com>
2024-01-08 15:22:00 +0530

    game completed, ready to play; Desktop optimized

--- webpages/stone-paper-scissorGame/stonePaperScissor.js
+++ webpages/stone-paper-scissorGame/stonePaperScissor.js
@@ -2,6 +2,10 @@ let userScore = 0;
 let compScore = 0;
 
 const choices =  document.querySelectorAll(".choice");
+const msg =  document.querySelector("#msg")
+const userScorePara = document.querySelector("#user-score")
+const compScorePara = document.querySelector("#comp-score")
+
 
 const genCompChoice = () => {
     const options = ["rock" , "paper", "scissors"];
@@ -11,17 +15,48 @@ const genCompChoice = () => {
 
 
 const drawGame = () => {
-    console.log('game was draw.')
+    msg.innerText = 'Game Draw. Play again!';
 }
+
+const showWinner = (userWin, userChoice, compChoice) =>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = 'green';
+    }else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You lose! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = 'red';
+
+    }
+}
+
 const playGame = (userChoice) => {
-    console.log("user Choice", userChoice);
     //Generate computer Choice
     const compChoice = genCompChoice(); 
-    console.log("comp choice", compChoice);
 
     if(userChoice === compChoice){
         //draw game
         drawGame();
+    }
+    else{
+        let userWin = true;
+        if(userChoice === 'rock'){
+            //scissors, paper
+            userWin = compChoice === "paper" ? false : true;
+        }
+        else if(userChoice === 'paper'){
+            //scissors, rock
+            userWin = compChoice === 'scissors' ? false : true; 
+        }
+        else{
+            //rock, paper
+            userWin = compChoice === 'rock' ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+
     }
 }
 
