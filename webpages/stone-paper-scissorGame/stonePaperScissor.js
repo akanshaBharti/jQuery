c92c4667a97617ef6d509e49068bff813610de57
Akansha <akanshabharti1213@gmail.com>
2024-01-08 09:08:34 +0530

    working on game functioning

--- webpages/stone-paper-scissorGame/stonePaperScissor.js
+++ webpages/stone-paper-scissorGame/stonePaperScissor.js
@@ -2,9 +2,33 @@ let userScore = 0;
 let compScore = 0;
 
 const choices =  document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+    const options = ["rock" , "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+
+const drawGame = () => {
+    console.log('game was draw.')
+}
+const playGame = (userChoice) => {
+    console.log("user Choice", userChoice);
+    //Generate computer Choice
+    const compChoice = genCompChoice(); 
+    console.log("comp choice", compChoice);
+
+    if(userChoice === compChoice){
+        //draw game
+        drawGame();
+    }
+}
+
+
 choices.forEach((choice) => {
-    choice.addEventListener('click', () => {
+    choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
-        console.log("choice was clicked", userChoice);
+        playGame(userChoice)
     });
 });
